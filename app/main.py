501166cffda1c5701a735b70123135fc0bde19db
jciepka <jakub.ciepka@gmail.com>
2020-01-05 12:10:26 +0100

    added nn as a second model

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow import keras
 from app.utils import plot_decision_regions
 import matplotlib.pyplot as plt
-
 
 # load data to dataframe
 df = pd.read_csv('./data/games.csv')
@@ -28,9 +29,25 @@ data_combined = np.vstack((data_train, data_test))
 labels_combined = np.hstack((labels_train, labels_test))
 
 # FIRST MODEL - KNN
-for i in range(7):
-    knn = KNeighborsClassifier(n_neighbors=i+1, p=2, metric='minkowski')
-    knn.fit(data_train, labels_train)
-    print('Test Accuracy: %.3f with number of neighbors: %d' %(knn.score(data_test, labels_test), i+1))
+# for i in range(7):
+#     knn = KNeighborsClassifier(n_neighbors=i + 1, p=2, metric='minkowski')
+#     knn.fit(data_train, labels_train)
+#     print('KNN Accuracy on test data: %.3f with number of neighbors: %d' % (knn.score(data_test, labels_test), i + 1))
 
 
+# SECOND MODEL - NN WITH KERAS
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(60,)),
+    keras.layers.Dense(16, activation=tf.nn.relu),
+    keras.layers.Dense(16, activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid),
+])
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(data_train, labels_train, epochs=5, batch_size=1)
+
+test_loss, test_acc = model.evaluate(data_test, labels_test)
+print('Test accuracy:', test_acc)
